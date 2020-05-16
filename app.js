f481f29a711193f794b961683f7bcd5b00d856a7
snbrathnayake <snbrathnayake@gmail.com>
2020-05-16 19:20:07 +0530

    change node server port : 8002

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-console.log('node server implement.!')
-
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(8001)
+}).listen(8002)
